39491c163b429ced03e5c4bb28d1061f8c4faac6
Pratyush Banerjee <pratyushbanerjee75@gmail.com>
2026-08-12 20:53:04 +0530

    twoSum_bruteforce

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-HashMap<Integer, Integer> map = new HashMap<>();
-for(int i =0;i<nums.length;i++){
-    int n = target - nums[i];
-    if(map.containsKey(n)){
-      return new int[]{map.get(n), i};
-    }
-    map.put(nums[i],i);
-}
 
- return new int[] {};
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
         }
 
+        return new int[]{};
     }
-
+}
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
